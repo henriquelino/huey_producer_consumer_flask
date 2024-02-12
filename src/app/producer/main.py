d4915b9599c180513ec9aaac8da3b514cbb0e138
Henrique Lino <henrique.lino97@gmail.com>
2024-02-12 01:43:55 -0300

    add version var, handle windows platform, setup logging for producer

--- src/app/producer/main.py
+++ src/app/producer/main.py
@@ -1,13 +1,23 @@
+__version__ = '0.1.0'
+
+import platform
 import sys
 from pathlib import Path
 
 from huey.api import Result  # noqa: F401
+from loguru import logger
 
-sys.path.append(str(Path('.').resolve().parent))
-import commons.tasks.example as tasks
+if platform.system() == 'Windows':
+    # add the upper folder to python path to be able to import commons folder
+    sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+import commons.tasks as tasks
+from commons import BASE_DIR, configfile
+from commons.log import setup_logging
 
 
 def main():
+    setup_logging(BASE_DIR, **configfile.get('log', {}))
     # create some tasks to just open an url
     # in reality we would generate tasks from an database
     # just sending the PK of the task
@@ -17,11 +27,13 @@ def main():
         'https://github.com/henriquelino',
         'https://github.com/henriquelino/autohotkey',
         'https://github.com/henriquelino/rpachallenge',
-        'https://github.com/henriquelino/configjy',
     ]
-    for url in urls:
-        print('creating task')
-        tasks.open_an_url(url)
+    logger.info(f'creating {len(urls)} tasks')
+    tasks.get_url.map(urls)
+
+    logger.info('creating 1 more task')
+    tasks.get_url('https://github.com/henriquelino/configjy')
+
     return
 
 
