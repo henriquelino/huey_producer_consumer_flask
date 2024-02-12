8661e4ef3dcc1574aa07520a5c3feaeae2c10b60
Henrique Lino <henrique.lino97@gmail.com>
2024-02-12 01:44:53 -0300

    append paths when running tests to fix python import error for commons folders

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,12 @@
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
-
+# expand python path so modules can import as if running on terminal/exe
+sys.path.append(str(Path('.').resolve()))
 sys.path.append(str(Path('.').resolve() / 'src' / 'app'))
+sys.path.append(str(Path('.').resolve() / 'src' / 'app' / 'consumer'))
+
 if sys.platform == "win32":
     # without this tox couldn't find chrome binary
     os.environ.update({"PROGRAMW6432": "C:\\Program Files"})
--- tests/consumer/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path('.').resolve() / 'src' / 'app' / 'consumer'))
