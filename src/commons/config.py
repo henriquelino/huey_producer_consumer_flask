181c000df41da66b0e82eebfdb6cfdae002a9730
Henrique Lino <henrique.lino97@gmail.com>
2024-02-18 12:07:35 -0300

    🎨 tox formatted

--- src/commons/config.py
+++ src/commons/config.py
@@ -31,10 +31,12 @@ db_path = Path(db_path).resolve()
 logger.critical(f"Using database in {db_path}")
 huey = SqliteHuey(filename=str(db_path))
 
+
 @huey.pre_execute()
 def pre_execute_hook(task):
-    logger.info(f"Pre execute on {task = }")
+    logger.info(f"Pre execute on {task.__dict__ = }")
+
 
 @huey.post_execute()
 def post_execute_hook(task, task_value, exc):
-    logger.info(f"Post execute on {task = }. {task_value = }. {exc = }")
+    logger.info(f"Post execute on {task.__dict__ = }. {task_value = }. {exc = }")
